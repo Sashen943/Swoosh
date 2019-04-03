cc32a784b68e89feb1244b10adef6f5f32435692
Sashen Pillay <sashenpillay943@gmail.com>
2019-04-03 09:16:49 +0200

    Refactored input

--- Swoosh/Swoosh/View/CanvasView.swift
+++ Swoosh/Swoosh/View/CanvasView.swift
@@ -10,41 +10,58 @@ import UIKit
 
 class CanvasView: UIView {
     
-    var strokes = [[CGPoint]]()
+    var lineColor: UIColor!
+    var lineWidth: CGFloat!
+    var path: UIBezierPath!
+    var touchPoint: CGPoint!
+    var startingPoint: CGPoint!
 
-    override func draw(_ rect: CGRect) {
-        
-        super.draw(rect)
-        
-        self.backgroundColor = .white
-        
-        let context = UIGraphicsGetCurrentContext() ?? nil
-        
-        for stroke in strokes {
-            for(i, s) in stroke.enumerated() {
-                if ( i == 0) {
-                    context?.move(to: s)
-                } else {
-                    context?.addLine(to: s)
-                }
-            }
-        }
-        
-        context?.strokePath()
+    override func layoutSubviews() {
+        self.isMultipleTouchEnabled = false
+        lineColor = UIColor.white
+        self.backgroundColor = UIColor.black
+        lineWidth = 10
     }
-    
 
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        guard let point = touches.first?.location(in: nil) else { return }
-        guard var lastStroke = strokes.popLast() else { return }
-        lastStroke.append(point)
-        strokes.append(lastStroke)
-        setNeedsDisplay()
-    }
-    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        strokes.append([CGPoint]())
+        let touch = touches.first
+        startingPoint = touch?.location(in: self)
     }
 
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = touches.first
+        touchPoint = touch?.location(in: self)
+        path = UIBezierPath()
+        path.move(to: startingPoint)
+        path.addLine(to: touchPoint)
+        startingPoint = touchPoint
+        drawShapeLayer()
+    }
+
+    func drawShapeLayer() {
+        let shapeLayer = CAShapeLayer()
+        shapeLayer.path = path.cgPath
+        shapeLayer.strokeColor = lineColor.cgColor
+        shapeLayer.lineWidth = lineWidth
+        shapeLayer.fillColor = UIColor.clear.cgColor
+        self.layer.addSublayer(shapeLayer)
+        self.setNeedsDisplay()
+    }
+
+    func clearCanvas() {
+        path.removeAllPoints()
+        self.layer.sublayers = nil
+        self.setNeedsDisplay()
+    }
+    
+}
+
+extension UIImage {
+    convenience init (view:UIView) {
+        UIGraphicsBeginImageContext(view.frame.size)
+        view.layer.render(in: UIGraphicsGetCurrentContext()!)
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        self.init(cgImage: image!.cgImage!)
+    }
 }
--- Swoosh/Swoosh/View/CanvasViewController.swift
+++ Swoosh/Swoosh/View/CanvasViewController.swift
@@ -1,19 +1,80 @@
-//
+
 //  CanvasViewController.swift
 //  Swoosh
-//
 //  Created by Sashen Pillay on 2019/03/24.
 //  Copyright © 2019 Sashen Pillay. All rights reserved.
 //
 
 import UIKit
+import Vision
 
 class CanvasViewController: UIViewController {
 
+    // MARK: @IBOutlets
+    
+    @IBOutlet weak var canvasView: CanvasView!
+    @IBOutlet weak var recognitionLabel: UILabel!
+    @IBOutlet weak var labelImageView: UIImageView!
+    
+    // MARK: Properties
+    
+    var requests = [VNRequest]()
+    
+    // MARK: Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-       
+        setupVision()
+    }
+    
+    // MARK: @IBActions
+    
+    @IBAction func clearCanvasTap(_ sender: Any) {
+        canvasView.clearCanvas()
+        recognitionLabel.text = ""
+    }
+    
+    @IBAction func recogniseTap(_ sender: Any) {
+        let image = UIImage(view: canvasView)
+        let scaledImage = scaleImage(image: image, toSize: CGSize(width: 28, height: 28))
+        let imageRequestHandler = VNImageRequestHandler(cgImage: scaledImage.cgImage!, options: [:])
+        
+        do {
+            try imageRequestHandler.perform(self.requests)
+            canvasView.clearCanvas()
+        }catch{
+            print(error)
+        }
+    }
+    
+    // MARK: Opertaions
+    
+    func setupVision() {
+        guard let visionModel = try? VNCoreMLModel(for: MNIST().model) else {fatalError("can not load Vision ML model")}
+        let classificationRequest = VNCoreMLRequest(model: visionModel, completionHandler: self.handleClassification)
+        self.requests = [classificationRequest]
+    }
+    
+    func scaleImage (image:UIImage, toSize size:CGSize) -> UIImage {
+        UIGraphicsBeginImageContextWithOptions(size, false, 1.0)
+        image.draw(in: CGRect(x: 0, y: 0, width: size.width, height: size.height))
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return newImage!
+    }
+    
+    func handleClassification (request:VNRequest, error:Error?) {
+        guard let observations = request.results else {print("no results"); return}
+        let classifications = observations
+            .compactMap({$0 as? VNClassificationObservation})
+            .filter({$0.confidence > 0.9})
+            .map({$0.identifier})
+    
+        let number = classifications.last
+        DispatchQueue.main.async {
+            self.recognitionLabel.text = number
+        }
+        
     }
 
 }
